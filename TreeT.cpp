7969ca02e716bb93504acb2f6c8759e9cb26b1bf
Yang Liu <liu.yang3.141@gmail.com>
2021-10-31 17:39:41 -0700

    Added ContainsHelper()

--- TreeT.cpp
+++ TreeT.cpp
@@ -1,3 +1,6 @@
+
+#include "TreeT.h"
+
 template<class T>
 TreeT<T>::TreeT() {
     root = nullptr;
@@ -6,76 +9,141 @@ TreeT<T>::TreeT() {
 
 template<class T>
 TreeT<T>::~TreeT() {
-
+    DestroyTree(root);
 }
 
 template<class T>
-TreeT &TreeT<T>::operator=(const TreeT &otherTree) {
-    return;
+TreeT<T> &TreeT<T>::operator=(const TreeT &otherTree) {
+    if (this == &otherTree)
+        return *this;
+
+    CopyHelper(this->root, &otherTree->root);
+
+    return *this;
 }
 
 template<class T>
 void TreeT<T>::Add(T value) {
-
+    /* Iterative implementation
     Node* curr = root;
-    Node* parent = nullptr;
 
-    while(curr != nullptr)  {
-        if (value == curr->value)
+    while (curr != nullptr) {
+        if (curr-> value == value)
             return;
-
-        if (value < curr->value)
+        else if (value < curr->value)
             curr = curr->left;
-
         else
             curr = curr->right;
-
-        parent = curr;
     }
-    Node* newNode = new Node;
 
-    if(parent == nullptr)   {
-        root = newNode; // if the first node is empty
-    }
-    if (value < parent->value)
-        parent->left = newNode;
-    else
-        parent->right = newNode;
-
-    ++numNodes;
+    curr = new Node;
     curr->value = value;
+    ++numNodes;*/
 }
 
 template<class T>
 void TreeT<T>::Remove(T value) {
+    RemoveHelper(root, value);
+}
 
+template<class T>
+void TreeT<T>::RemoveHelper(TreeT::Node *&subtree, T value) {
+    // this function searches for where the value is in the tree
+    // if it's not on the right or the left, then we've found it, then call DeleteNode()
+    if (value < subtree->value)
+        RemoveHelper(subtree->left, value);
+    else if (value > subtree->value)
+        RemoveHelper(subtree->right, value);
+    else
+        DeleteNode(subtree);
+}
+
+template<class T>
+void TreeT<T>::DeleteNode(TreeT::Node *&subtree) {
+    T data;
+    Node* tempPtr;
+    tempPtr = subtree;
+
+    if (subtree->left == nullptr && subtree->right == nullptr)  {
+        delete subtree;
+        subtree == nullptr;
+    }
+    else if (subtree->left == nullptr)  {
+        subtree = subtree->right;
+        delete tempPtr;
+    }
+    else if (subtree->right == nullptr) {
+        subtree = subtree->left;
+        delete tempPtr;
+    }
+    else    {
+        GetPredecessor(subtree->left, data);
+        subtree->info = data;
+        RemoveHelper(subtree->left, data);
+    }
+}
+
+template<class T>
+void TreeT<T>::GetPredecessor(TreeT::Node *curr, T &value) {
+    while (curr->right != nullptr)
+        curr = curr->right;
+
+    value = curr->value;
 }
 
 template<class T>
 bool TreeT<T>::Contains(T value) {
-    Node* curr = new Node;
-    curr = root;
+    return ContainsHelper(root, value);
 
-    while(curr != nullptr)  {
-        if (value == curr->value)
+    /* Iterative implementation
+    Node *curr = root;
+
+    while (curr != nullptr) {
+        if (curr->value == value)
             return true;
-
-        if (value < curr->value)
+        else if (value < curr->value)
             curr = curr->left;
         else
             curr = curr->right;
     }
 
+    // If root is null or value not found in while loop, then jump here.
     return false;
+    */
+}
+
+template<class T>
+bool TreeT<T>::ContainsHelper(TreeT::Node *subroot, T value) {
+    if (subroot == nullptr)
+        return false;
+
+    else if (subroot->value == value)
+        return true;
+
+    else if (value < subroot->value)
+        return ContainsHelper(subroot->left, value);
+
+    else
+        return ContainsHelper(subroot->right, value);
+
 }
 
 template<class T>
 int TreeT<T>::Size() {
-    return 0;
+    return numNodes;
 }
 
 template<class T>
 void TreeT<T>::ResetIterator(Order traverseOrder) {
+    iterArr.empty();
+    if (traverseOrder == 0) {
+        PlacePostOrder(root);
+    }
+    else if (traverseOrder == 1)    {
+        PlaceInOrder(root);
+    }
+    else
+        PlacePreOrder(root);
 
 }
 
@@ -86,40 +154,46 @@ T TreeT<T>::GetNextItem() {
 
 template<class T>
 void TreeT<T>::DestroyTree(TreeT::Node *node) {
-
-}
-
-template<class T>
-void TreeT<T>::RemoveHelper(TreeT::Node *&subtree, T value) {
-
-}
-
-template<class T>
-void TreeT<T>::DeleteNode(TreeT::Node *&subtree) {
-
-}
-
-template<class T>
-void TreeT<T>::GetPredecessor(TreeT::Node *curr, T &value) {
-
+    DestroyTree(node->left);
+    DestroyTree(node->right);
+    delete node;
 }
 
 template<class T>
 void TreeT<T>::CopyHelper(TreeT::Node *&thisTree, TreeT::Node *otherTree) {
-
+    thisTree->value = otherTree->value;
+    CopyHelper(thisTree->left, otherTree->left);
+    CopyHelper(thisTree->right, otherTree->right);
 }
 
 template<class T>
 void TreeT<T>::PlacePreOrder(TreeT::Node *node) {
+    if (node == nullptr)
+        return;
 
+    iterArr.push(node->value);
+    PlacePreOrder(node->left);
+    PlacePreOrder(node->right);
 }
 
 template<class T>
 void TreeT<T>::PlacePostOrder(TreeT::Node *node) {
+    if (node == nullptr)
+        return;
 
+    PlacePostOrder(node->left);
+    PlacePostOrder(node->right);
+    iterArr.push(node->value);
 }
 
 template<class T>
 void TreeT<T>::PlaceInOrder(TreeT::Node *node) {
+    if (node == nullptr)
+        return;
 
+    PlaceInOrder(node->left);
+    iterArr.push(node->value);
+    PlaceInOrder(node->right);
 }
+
+
